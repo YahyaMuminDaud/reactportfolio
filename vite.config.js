880769b894c063020170cd6bde08f683fee8bbf5
Yahya Daud <yahyamdaud21@gmail.com>
2025-01-21 23:46:37 -0800

    one more

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/reactportfolio/",  // Update the base path if needed for GitHub Pages or other environments
-  build: {
-    // Removed 'external' for 'react-router-dom' because it should be bundled with the app
-  },
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      // other aliases you may need
-    }
-  }
+  base: "/reactportfolio/",
 });
 
