abb060da45c35fe64e35127ac83722729c8e5aae
Yahya Daud <yahyamdaud21@gmail.com>
2025-01-22 02:01:25 -0800

    final phone update

--- src/recruit.jsx
+++ src/recruit.jsx
@@ -46,11 +46,16 @@ const Recruit = () => {
             <button onClick={() => navigate('/')} className="Home" id="button">Home</button>
             <h2 id="Text" className="intro2">Hello! <br /> I've got a question.</h2>
             <p id="Text" className="intro3">If you were a student how would you stand out? <br />If you met me: What could I improve on in standing out? Any tips? <br />If you haven't met me: What is a factor that catches you eye as a recruiter? <br />If you aren't a recruiter: How are you standing out?</p>
+            <h2 id="Text" className="intro4">Hello! <br /> I've got a question.</h2>
+            <p id="Text" className="intro5">If you were a student how would you stand out? <br />If you met me: What could I improve on in standing out? Any tips? <br />If you haven't met me: What is a factor that catches you eye as a recruiter? <br />If you aren't a recruiter: How are you standing out?</p>
             <form method="POST">
                 <input className="name" type="text" name="Name" placeholder="Enter Name" value={user.Name} autoComplete="off" required onChange={data} />
                 <input className="email" type="email" name="Email" placeholder="Enter Email" value={user.Email} autoComplete="off" required onChange={data} />
                 <textarea className="Answer" name="message" placeholder="Enter Answer" value={user.message} autoComplete="off" required onChange={data}></textarea>
+                <textarea className="Answer2" name="message" placeholder="Enter Answer" value={user.message} autoComplete="off" required onChange={data}></textarea>
                 <button id="button" className="submission" onClick={getData}>Submit</button>
+                <button id="button" className="submission2" onClick={getData}>Submit</button>
+
             </form>
             <div className="logo"></div>
             <div className="line"></div>
