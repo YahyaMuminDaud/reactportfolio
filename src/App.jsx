c56470244853df6d4cdcdb9c663af684a81365de
Yahya Daud <yahyamdaud21@gmail.com>
2025-01-21 23:30:27 -0800

    it may be time

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import Home from './home.jsx';
 import About from './about.jsx';
 import Projects from './projects.jsx';
 import Recruit from './recruit.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/recruit" element={<Recruit />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/recruit" element={<Recruit />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
