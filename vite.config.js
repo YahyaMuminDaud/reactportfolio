5a2a35bdb9c87954b5ae39e818fae7cffc2ea8d8
Yahya <yahyamdaud21@gmail.com>
2025-02-28 01:56:59 -0800

    My own phone fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/reactportfolio/",
+  server: {
+    host: '0.0.0.0',  // Exposes the server to your network
+    port: 5174,       // Use the same port as shown in your terminal
+    strictPort: true  // Ensures Vite doesn't auto-change the port
+  }
 });
-
